3dd5fd820f651b8f8482dab72fe50862a5904a2c
Boeing13 <39527481+Boeing13@users.noreply.github.com>
2018-10-18 22:28:21 +0700

    Update GUI with login form, prototype of main screen

--- PROJECT_BookingTicket/GUI/ManageUser.Designer.cs
+++ PROJECT_BookingTicket/GUI/ManageUser.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.btnDetailUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -41,14 +45,46 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "User Management";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "List of all Customers";
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(20, 121);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.Size = new System.Drawing.Size(607, 345);
+            this.dgvUsers.TabIndex = 2;
+            // 
+            // btnDetailUser
+            // 
+            this.btnDetailUser.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDetailUser.Location = new System.Drawing.Point(650, 121);
+            this.btnDetailUser.Name = "btnDetailUser";
+            this.btnDetailUser.Size = new System.Drawing.Size(95, 34);
+            this.btnDetailUser.TabIndex = 3;
+            this.btnDetailUser.Text = "Detail ...";
+            this.btnDetailUser.UseVisualStyleBackColor = true;
+            // 
             // ManageUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.btnDetailUser);
+            this.Controls.Add(this.dgvUsers);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "ManageUser";
-            this.Size = new System.Drawing.Size(748, 494);
+            this.Size = new System.Drawing.Size(762, 500);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +93,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Button btnDetailUser;
     }
 }
