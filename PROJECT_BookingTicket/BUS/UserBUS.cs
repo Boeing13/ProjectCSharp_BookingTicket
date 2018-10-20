685073f17ffc2a46b2083909ee8a11d674bfa4f5
Boeing13 <39527481+Boeing13@users.noreply.github.com>
2018-10-20 13:23:33 +0700

    Update User business logic, change namespace

---
+++ PROJECT_BookingTicket/BUS/UserBUS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entity;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BUS
+{
+    
+    public class UserBUS
+    {
+        UserDAO dao = new UserDAO();
+
+
+        public DataTable ShowUser()
+        {
+            string query = "select u.userID, u.fullName, u.tel, u.address  from [User] u";
+            DataTable dt = new DataTable();
+            dt = dao.GetTable(query);
+            return dt;
+        }
+
+
+    }
+}
