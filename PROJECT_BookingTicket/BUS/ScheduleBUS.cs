76ccd6bd51619c78e12071b273c6ed2b3c4ce9d9
Boeing13 <39527481+Boeing13@users.noreply.github.com>
2018-10-22 20:09:42 +0700

    Update DAL, BUS for admin management

---
+++ PROJECT_BookingTicket/BUS/ScheduleBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DAL;
+
+namespace BUS
+{
+    public class ScheduleBUS
+    {
+        ScheduleDAO dao = new ScheduleDAO();
+
+        public DataTable SelectAll()
+        {
+            string query = "select r.name, h.time, m.title, s.date, s.leftSeat  from Schedule s " +
+                             " inner join Room r on s.roomID = r.roomID " +
+                             " inner join Movie m on s.movieID = m.movieID " +
+                             " inner join[Hour] h on s.hourID = h.hourID ";
+            DataTable dt = new DataTable();
+            dt = dao.GetTable(query);
+            return dt;
+        }
+    }
+}
---
+++ PROJECT_BookingTicket/DAL/ScheduleDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DAL
+{
+    public class ScheduleDAO
+    {
+        public DataTable GetTable(string query)
+        {
+            SqlConnection conn = DBContext.getConnection();
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+
+        public void NonExecuteQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            cmd.Clone();
+        }
+    }
+}
